570221d0a6507a02c0e8f138b6862f2f99661178
AMAN7475 <amanraikwar1996@gmail.com>
2025-12-20 21:23:21 +0530

    program added - arithematic_calculation.py

---
+++ arithematic_calculation.py
@@ -0,0 +1,16 @@
+a = int(input("Enter value a :"))
+b = int(input("Enter value b :"))
+c = a + b
+print(c)
+
+d = a - b
+print(d)
+
+e = a * b
+print(e)
+
+f = a / b
+print(f)
+
+g = a % b
+print(g)
