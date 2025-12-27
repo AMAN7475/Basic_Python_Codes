19e900cb6635f3c76016bcb8e94b4b8df0fcec41
AMAN7475 <amanraikwar1996@gmail.com>
2025-12-27 21:08:06 +0530

    Program added - fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,22 @@
+def Fibonacci():
+	n = int(input("How many fibonacci numbers you want to generate?: "))
+	i = 1
+
+	if n == 0:
+		fib = []
+
+	elif n == 1:
+		fib = [1]
+
+	elif n == 2:
+		fib = [1,1]
+
+	elif n > 2:
+		fib = [1,1]
+		while i < (n-1):
+			fib.append(fib[i] + fib[i-1])
+			i += 1
+
+	return (fib)				
+
+print(Fibonacci())	
