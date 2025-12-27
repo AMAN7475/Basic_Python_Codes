5e023af27a43adb1d46b60516c2e9b57965bb8c2
AMAN7475 <amanraikwar1996@gmail.com>
2025-12-27 21:04:30 +0530

    Program added - electricity bill

---
+++ electricity_bill.py
@@ -0,0 +1,50 @@
+"""def ec_2(unit):
+
+	unit >= 101 and <= 200:
+		return (unit*7)
+
+	elif unit >= 201 and <= 300:
+		return (unit*9)
+
+	else:
+		return (unit*10)		
+
+
+x = int(input("Electricity Consumption (in units) : "))
+
+for i in range(1,x+1):
+	if x <= 100:"""
+
+"""unit = int(input("Electricity Consumption (in units) : "))
+bill = 0 
+
+if unit <= 100:
+	bill = (unit*5)
+	print("Bill",bill)
+
+elif unit <= 200:
+	bill = (100*5) + ((unit-100)*7)
+	print("Bill",bill)
+
+elif unit <= 300:
+	bill = (100*5) + (100*7) + ((unit-200)*9)
+	print("Bill",bill)
+
+else:
+	bill = (100*5) + (100*7) + (100*9) + ((unit-300)*10)
+	print("Bill",bill)"""
+
+
+"""p = int(input("Principal : "))
+r = int(input("Rate : "))
+n = int(input("Compounding Time : "))
+
+
+a = p*((1+(r/100))**n)
+
+Compound_interest = a - p 
+
+Compound_interest = int(Compound_interest)
+
+print("Compound_interest",Compound_interest)"""
+
